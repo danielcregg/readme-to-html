0649c7a329eab7254de87065375b75d7d0bf1d4a
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-03-26 13:07:39 +0000

    Refactor README fetching logic to use raw GitHub URL

--- convert.php
+++ convert.php
@@ -10,16 +10,23 @@ if(isset($_GET['url'])) {
         exit;
     }
 
-    // Fetch README contents
-    $readme_content = @file_get_contents($_GET['url']);
+    // Extract the raw GitHub URL
+    $urlParts = explode('/', $_GET['url']);
+    if(count($urlParts) < 5) {
+        http_response_code(400);
+        echo "Error: Invalid GitHub URL.";
+        exit;
+    }
+    $rawReadmeURL = "https://raw.githubusercontent.com/" . $urlParts[3] . "/" . $urlParts[4] . "/main/README.md";
 
+    // Fetch README contents
+    $readme_content = @file_get_contents($rawReadmeURL);
     // Check if fetching was successful
     if ($readme_content === false) {
         http_response_code(500);
         echo "Error: Failed to fetch README.";
         exit;
     }
-
     // Parse README
     $Parsedown = new Parsedown();
     echo $Parsedown->text($readme_content);
